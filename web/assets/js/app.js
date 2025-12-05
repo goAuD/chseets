bda441b37bec282a41227d43c10d8e3df9684fd9
goaud <goaud@users.noreply.github.com>
2025-12-05 23:23:13 +0100

    modified: sw.js, app.js

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,5 +1,5 @@
 // /assets/js/app.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const PDFJS_CDN = "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.4.168/build/pdf.min.mjs";
 
 /* ---------------- Supabase ---------------- */
